728b109f0c8ccfe38eea108150d1f3b4023129c6
akradadiya28 <akradadiya28@gmail.com>
2024-11-27 21:32:12 +0530

    var-const added

---
+++ 01_var-const/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score = 20;
+    const int uid = 20;
+    int ak_balance = 200;
+
+    // uid = 100;
+
+    cout << "Hello from chai" << endl;
+    return 0;
+}
